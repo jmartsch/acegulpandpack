28074d4e9a4b25ba2fb68ed663a6bff5798817f2
Jens Martsch <jmartsch@gmail.com>
2020-11-20 22:18:48 +0100

    add Eslint and uglify
    add scriptCopy task

--- gulpfile.babel.js/tasks/scripts.js
+++ gulpfile.babel.js/tasks/scripts.js
@@ -12,23 +12,40 @@ import gulpWebpack from 'webpack-stream';
 import gulpEslint from 'gulp-eslint';
 import plumber from 'gulp-plumber';
 import errorHandler from '../util/errorHandler.js';
+import uglify from 'gulp-uglify';
 
 // Config
 import { config, isProd } from '../config';
 
+function isFixed(file) {
+  // Has ESLint fixed the file contents?
+  return file.eslint != null && file.eslint.fixed;
+}
+
+let webpackConfigName = 'webpack.dev.js';
+
+if (isProd) webpackConfigName = 'webpack.prod.js';
+
 // Task
 export function esTranspile() {
   return src(config.paths.scripts.src)
-    .pipe(plumber({errorHandler}))
-    .pipe(gulpWebpack(require('../../webpack.config.js'), webpack))
+    .pipe(plumber({ errorHandler }))
+    .pipe(gulpWebpack(require(`../../${webpackConfigName}`), webpack))
     .pipe(dest(config.paths.scripts.dest));
 }
 
 export function esLint() {
   return src(config.paths.scripts.src)
-    .pipe(gulpEslint())
-    .pipe(gulpEslint.format())
-    .pipe(gulpif(isProd, gulpEslint.failAfterError()));
+    .pipe(gulpEslint({ fix: true }))
+    .pipe(gulpEslint.formatEach());
+  // .pipe(gulpif(isProd, gulpEslint.failAfterError()));
 }
 
-export const scripts = series(esLint, esTranspile);
+export function scriptCopy() {
+  return src(config.paths.copy.src)
+    // .pipe(changed(config.paths.copy.dest))
+    .pipe(uglify())
+    .pipe(dest(config.paths.copy.dest));
+}
+
+export const scripts = series(esTranspile, scriptCopy);
